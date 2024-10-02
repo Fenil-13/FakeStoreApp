baed5a130119cfde8ce89318a72b4977f117df96
Fenil Moradiya <53660680+Fenil-13@users.noreply.github.com>
2024-10-02 21:44:39 +0530

    Initial commit

---
+++ app/src/main/java/com/fenil/starterprojectandroid/StarterApplication.kt
@@ -0,0 +1,9 @@
+package com.fenil.starterprojectandroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StarterApplication : Application() {
+
+}
